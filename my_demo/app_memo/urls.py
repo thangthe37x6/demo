f4aaf677dbab961b5773bdbd5f7784b86756161e
Thế Thắng <conga200903@gmail.com>
2023-03-07 11:27:39 +0700

    login and register

--- my_demo/app_memo/urls.py
+++ my_demo/app_memo/urls.py
@@ -2,5 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index, name="index"),
+
+
+
+
+    path('register/', views.register, name='register'),
+    path('loginpage/', views.login, name='login'),
+    
 ]
--- my_demo/app_memo/views.py
+++ my_demo/app_memo/views.py
@@ -1,6 +1,31 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
 
+#create your views here
+from .models import *
+from .forms import createUserForm
+
+
+def register(request):
+    template = loader.get_template('Register.html')
+    form = createUserForm()
+    if request.method == 'POST':
+        form = createUserForm()
+        if form.is_valid():
+            form.save()
+    context = {
+        'form': form
+    }
+    return HttpResponse(template.render(context,request))
+
+def login(request):
+    template = loader.get_template('login.html')
+    
+    context = {
+
+    }
+    return HttpResponse(template.render(context))
 
 def index(request):
     template = loader.get_template('testing.html')
